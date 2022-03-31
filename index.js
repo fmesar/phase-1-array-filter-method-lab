0d63280cb7899a6b977ba2fe4fc104a503332ad5
Francisco Mesar <fmesar87@gmail.com>
2022-03-31 15:14:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name){
+     return drivers.filter(element => element == name.toLowerCase() || element == name)
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(element => element.indexOf(name) !== -1 && element.charAt(0) === name.charAt(0))
+}
+
+
+function matchName(drivers, name){
+    return drivers.filter(element => element['name'] == name)
+}
